76564d546873d461559e9c3c18df9751660f30ca
brett.cole <acodercalledcole@gmail.com>
2023-09-07 10:25:01 -0400

    arrays and tuples

--- demo/arrays/arrays.ts
+++ demo/arrays/arrays.ts
@@ -10,3 +10,44 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const numbers: number[] = [1, 2, 3];
+let letters: string[] = ["a", "b", "c"];
+
+letters = ["a", "b", "c"];
+
+const b = letters[1];
+assert.equal(b, "b");
+
+letters[1] = "z";
+assert(letters[1] === "z");
+assert.deepEqual(letters, ["a", "z", "c"]);
+
+// try to avoid this
+const mixed = [1, "hello"];
+
+const names = [
+    ["Brett", "C"],
+    ["Rachel", "C"]
+];
+
+const brett = names[0][0];
+const c = names[0][1];
+const b2 = names[1][1];
+console.log(brett);
+console.log(c);
+console.log(b2);
+
+const nums: number[] = [];
+// add element to end of array
+nums.push(10);
+nums.push(20);
+nums.push(30);
+assert.deepEqual(nums, [10, 20, 30]);
+
+// remove last element
+nums.pop();
+assert.deepEqual(nums, [10, 20]);
+
+nums.splice(0, 1);
+assert.deepEqual(nums, [20]);

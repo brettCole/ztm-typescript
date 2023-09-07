73a682c52812ad337e7446ffeebd5d36bf2b76e9
brett.cole <acodercalledcole@gmail.com>
2023-09-07 07:37:36 -0400

    arrays, arrow functions and objects

--- demo/array-object/array-object.ts
+++ demo/array-object/array-object.ts
@@ -1,2 +1,27 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+
+type Link = {
+    title: string;
+    url: string;
+};
+
+const microsoft = {
+    title: "microsoft",
+    url: "microsoft.com",
+};
+
+const typescript = {
+    title: "TypeScript",
+    url: "typescript.com"
+};
+
+//              0           1
+const links = [microsoft, typescript];
+
+const tsUrl = links[1].url;
+
+links[0].title = "Microsoft";
+
+console.log(tsUrl);
+console.log(links[0]);

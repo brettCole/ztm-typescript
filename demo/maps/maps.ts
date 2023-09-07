2a462155ffced0f8650b7f1b492d783aa562b64d
brett.cole <acodercalledcole@gmail.com>
2023-09-07 15:00:47 -0400

    iterations and Map

--- demo/maps/maps.ts
+++ demo/maps/maps.ts
@@ -11,3 +11,30 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+type Name = string;
+type Score = number;
+
+//                     <KEY,   VALUE>
+const testScores: Map<Name, Score> = new Map();
+
+testScores.set("Alice", 96);
+testScores.set("Bob", 86);
+testScores.set("Carol", 92);
+
+assert.equal(testScores.size, 3);
+
+//          [KEY, VALUE]
+for (const [name, score] of testScores) {
+    console.log(`${name} score is ${score}`);
+}
+
+testScores.delete("Bob");
+assert.equal(testScores.size, 2);
+
+assert.equal(testScores.has("Bob"), false);
+
+for (const name of testScores.keys()) {}
+for (const score of testScores.values()) {}
+
+testScores.clear();

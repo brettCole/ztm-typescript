8688437b63d4b2b551478094b1890a811c30156b
brett.cole <acodercalledcole@gmail.com>
2023-09-07 12:34:46 -0400

    array exercises

--- exercise/arrays/2.ts
+++ exercise/arrays/2.ts
@@ -28,7 +28,7 @@ const vendors = [
 
 
 // perform step 1 here
-const smallSpaces = []; // ...
+const smallSpaces = vendors.filter(el => el.size <= 10);
 
 assert.deepStrictEqual(smallSpaces, (() => {
   const cloned = Array.from(vendors);
@@ -37,12 +37,14 @@ assert.deepStrictEqual(smallSpaces, (() => {
 })());
 
 // perform step 2 here
-const sizeExists = []; // ...
-
+const sizeExists = vendors.some(el => el.size === 13);
 assert.equal(sizeExists, true);
 
 // perform step 3 here
-const updatedDuration = []; // ...
+const updatedDuration = vendors.map(el => ({
+  ...el,
+  presentationDuration: el.presentationDuration + 3,
+}));
 
 assert.deepStrictEqual(updatedDuration,
   [

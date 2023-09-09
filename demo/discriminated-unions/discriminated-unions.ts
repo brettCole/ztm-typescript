6d21a7ddf5f7a399fe586d000317ed68ccca37cb
brett.cole <acodercalledcole@gmail.com>
2023-09-08 23:26:13 -0400

    discriminated-unions

--- demo/discriminated-unions/discriminated-unions.ts
+++ demo/discriminated-unions/discriminated-unions.ts
@@ -1,5 +1,7 @@
 /* eslint-disable */
 
+import { Z_ASCII } from "zlib";
+
 // Discriminated unions are a way to declare a type that can have
 // different properties or behaviors based on a specific discriminator property.
 // Discriminated unions can be defined using the intersection of a set of types
@@ -10,3 +12,42 @@
 // a 'discriminated union'. Unions only allow the type to be one
 // option at a time. Combining this with objects allows multiple
 // pieces of data to be associated with each individual option.
+
+type numbers = 1 | 2 | string | number;
+
+type AccountCreationMessage = 
+    | { kind: "ok"; greeting: string }
+    | { kind: "passwordComplexityFailure"; message: string }
+    | { kind: "usernameExists"; }
+
+const ok: AccountCreationMessage = {
+    kind: "ok",
+    greeting: "Welcome to the platform!"
+};
+
+const passwordTooShort: AccountCreationMessage = {
+    kind: "passwordComplexityFailure",
+    message: "Password must be at least 10 characters",
+};
+
+const exists: AccountCreationMessage = {
+    kind: "usernameExists"
+}
+
+function showMessage(msg: AccountCreationMessage) {
+    switch (msg.kind) {
+        case "ok":
+            console.log(`${msg.greeting}`);
+            break;
+        case "passwordComplexityFailure":
+            console.log(`${msg.message}`);
+            break;
+        case "usernameExists":
+            console.log("That username already exists");
+            break;
+    }
+}
+
+showMessage(ok);
+showMessage(passwordTooShort);
+showMessage(exists);

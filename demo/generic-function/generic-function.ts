dacee419f2d3bedb5c9714a8d33c43b6a8e4e142
brett.cole <acodercalledcole@gmail.com>
2023-09-09 08:18:45 -0400

    Generic functions and classes

--- demo/generic-function/generic-function.ts
+++ demo/generic-function/generic-function.ts
@@ -11,3 +11,45 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/generics.html#hello-world-of-generics
+
+function getFirstNumber(arr: number[]): number | undefined {
+    if (arr.length > 0) {
+        return arr[0];
+    }
+    return undefined;
+}
+
+function getFirstString(arr: string[]): string | undefined {
+    if (arr.length > 0) {
+        return arr[0];
+    }
+    return undefined;
+}
+
+interface Sample {}
+
+function getFirstStringOrNumber(
+    arr: (string | number | boolean | bigint)[]
+ ): string | number | boolean | bigint | undefined {
+    if (arr.length > 0) {
+        return arr[0];
+    }
+    return undefined;
+}
+
+getFirstStringOrNumber(["a", "b"]);
+getFirstStringOrNumber([1, 2]);
+
+// generic functions
+function getFirst<T>(arr: T[]): T | undefined {
+    if (arr.length > 0) {
+        console.log(arr[0]);
+        return arr[0];
+    }
+    return undefined;
+}
+
+getFirst([1, 2]);
+getFirst(['a', 'b']);
+getFirst([true, false]);
+getFirst([{}, {}]);

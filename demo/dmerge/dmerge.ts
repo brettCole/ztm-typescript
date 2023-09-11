3a3f8a51d33882ccd8d48c5b251641e4cc0d134d
brett.cole <acodercalledcole@gmail.com>
2023-09-10 20:29:20 -0400

    dmerge

--- demo/dmerge/dmerge.ts
+++ demo/dmerge/dmerge.ts
@@ -9,3 +9,31 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/declaration-merging.html
+
+// Define a simple interface for a user
+interface User {
+  name: string;
+  email: string;
+}
+
+// Extend the User interface with additional properties
+interface User {
+  age: number;
+  address: string;
+}
+
+// Define a function that takes a User object and returns their name and age
+function getUserInfo(user: User) {
+  return `${user.name} is ${user.age} years old.`;
+}
+
+// Create a user object with all the properties of the merged interfaces
+const user: User = {
+  name: "Brett Cole",
+  email: "acodercalledcole@gmail.com",
+  age: 37,
+  address: '123 Main St',
+};
+
+// Call the function to get the user's name and age
+console.log(getUserInfo(user)); // Output: Brett Cole is 30 years old.

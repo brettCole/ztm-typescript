a5aaf0ba8d52536396c11f8a4be52abcb9720570
brett.cole <acodercalledcole@gmail.com>
2023-09-10 20:33:02 -0400

    modules

---
+++ demo/modules/coord.ts
@@ -0,0 +1,9 @@
+// Interfaces can be exported:
+export interface Point {
+  x: number;
+  y: number;
+}
+
+// Default exported item. Prefer to NOT use this because it can break code
+// completion.
+export default Point;
--- demo/modules/modules.ts
+++ demo/modules/modules.ts
@@ -7,3 +7,34 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/modules.html
+
+// import individual items
+import { add, sub, pi } from "./math.ts";
+
+// rename an import
+import { Vec as Vector } from "./math.ts";
+
+// Use the "default" import (no curly braces) allows you to import the default
+// exported item in the module, while also renaming it at the same time. You'll
+// see this often, but try not to use it in your own code because of
+// discoverability problems:
+import Coordinate from "./coord.ts";
+// Prefer this instead of the above:
+// import { Point as coordinate } from "./coord.ts";
+
+// Once imported, the items can be used as normal:
+const four = add(2, 2);
+assert.equal(four, 4);
+
+const ten = sub(12, 2);
+assert.equal(ten, 10);
+
+const circleArea = pi * 10 ** 2;
+
+const letters = new Vector<string>();
+letters.push("a");
+letters.push("b");
+letters.push("c");
+
+
+const origin: Coordinate = { x: 0, y: 0 };
